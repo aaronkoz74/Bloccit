e4fcd3e1da572fe2f72a9eadb655547a9e7cf496
aaronkoz74 <aaronkoz74@gmail.com>
2015-11-12 22:21:05 -0500

    Assignment 31 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each_with_index do |post, id|
+        if id % 5 == 0
+          post.update(title: "SPAM")
+        end
+    end
   end
 
   def show
