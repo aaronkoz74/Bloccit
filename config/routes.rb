991a837f30c50a396644c2c058b8049e0da9c6ea
aaronkoz74 <aaronkoz74@gmail.com>
2015-11-13 14:01:02 -0500

    comments restful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
   resources :posts
+  # get '/posts' => 'posts#index'
+  # get '/posts/new' => 'posts#new'
+  # get '/posts/:id' => 'posts#show'
+  # get '/posts/:id/edit' => 'posts#show'
+  # post '/posts/' => 'posts#create'
+  # put '/posts/:id' => 'posts#update'
+  # delete '/posts/:id' => 'posts#destroy'
 
   get 'about' => 'welcome#about'
 
