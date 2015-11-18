6f770ad61cb7fdfbe7ffaace782cfe6c000e6b98
aaronkoz74 <aaronkoz74@gmail.com>
2015-11-18 14:43:29 -0500

    Checkpoint 38 partial

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   # get '/posts' => 'posts#index'
   # get '/posts/new' => 'posts#new'
   # get '/posts/:id' => 'posts#show'
