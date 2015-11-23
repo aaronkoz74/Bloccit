561919e8faa56bafcc42efd8f82f57d9438674f7
aaronkoz74 <aaronkoz74@gmail.com>
2015-11-23 10:21:29 -0500

    Checkpoint 40 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase }
-  # before_save { self.name = name.to_s.split.each { |name|
-            #   name.capitalize!}.join(' ')
-            # }
+  before_save { self.role ||= :member }
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -18,4 +16,6 @@ class User < ActiveRecord::Base
             format: { with: EMAIL_REGEX }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,48 @@ RSpec.describe User, type: :model do
       user.save
       expect(user.name).to eq "Bloccit User"
     end
+
+    it "should respond to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "should respond ti admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "should respond to member?" do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe "roles" do
+    it "should be member by default" do
+      expect(user.role).to eql("member")
+    end
+
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+    end
   end
 
   describe "invalid user" do
