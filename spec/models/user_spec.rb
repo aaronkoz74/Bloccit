5938fcb26dd5122bb97d325460afef7942611b18
aaronkoz74 <aaronkoz74@gmail.com>
2015-11-30 11:23:12 -0500

    Checkpoint 44 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { should have_many(:posts) }
   it { should have_many(:comments) }
   it { should have_many(:votes) }
+  it { should have_many(:favorites) }
   # Shoulda tests for name
   # it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
@@ -95,20 +97,21 @@ RSpec.describe User, type: :model do
     it "should be an invalid user due to incorrectly formatted email address" do
       expect(user_with_invalid_email_format).to_not be_valid
     end
-
   end
 
-  # describe "improperly-formatted user name" do
-  #   let(:user_with_invalid_name) { User.new(name: "inValIdformaT", email: "user@bloccit.com") }
-  #   let(:user_with_invalid_name_format) { User.new(name: "inValid forMat", email: "user@bloccit.com") }
-  #
-  #   it "should be an invalid user due to invalid format" do
-  #     expect(user_with_invalid_name).to_not be_valid
-  #   end
-  #
-  #   it "should be an invalid user due to incorrectly formatted name" do
-  #     expect(user_with_invalid_name).to_not be_valid
-  #   end
-  # end
+  describe "#favorite_for(post)" do
+    before do
+      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+    end
 
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(user.favorite_for(@post)).to be_nil
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = user.favorites.where(post: @post).create
+      expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end
 end
