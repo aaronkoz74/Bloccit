6448bb1bf35fe33fc2a5ad858a7e9f72813b0a41
aaronkoz74 <aaronkoz74@gmail.com>
2015-12-07 11:41:15 -0500

    Assignment 48 complete

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::PostsController < Api::V1::BaseController
+  before_filter :authenticate_user, except: [:show, :index]
+  before_filter :authorize_user, except: [:show, :index]
+
+  def show
+    post = Post.find(params[:id])
+    render json: post.to_json, status: 200
+  end
+
+  def index
+    posts = Post.all
+    render json: posts.to_json, status: 200
+  end
+end
