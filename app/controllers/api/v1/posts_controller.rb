4ee003ae4a7834bf7382fffca849e5b69336d6e0
aaronkoz74 <aaronkoz74@gmail.com>
2015-12-07 15:03:31 -0500

    Assignment 48 completed, added post and comment arrays

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,6 +9,6 @@ class Api::V1::PostsController < Api::V1::BaseController
 
   def index
     posts = Post.all
-    render json: posts.to_json, status: 200
+    render json: posts.to_json(include: :comments), status: 200
   end
 end
